c63fa87d266baec68f093f55935caea44b652656
louismeunier <louismeunier123@gmail.com>
2021-04-25 17:19:50 -0400

    Change pathing in mysql

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -7,27 +7,27 @@ mysql -u root --password="" < scripts/sql/init_db.sql
 for file in $(find ~/owl_data/2018 -name "*.csv"); do 
 	echo "'$file' -> db" ; 
 	mysql -u root --password="" \
-	-e "set @file='$file', @table='owl2018'; source scripts/sql/old_players_data.sql"
+	-e "set @file='$file', @table='owl2018'; source ~/scripts/sql/old_players_data.sql"
 done
 
 for file in $(find ~/owl_data/2019 -name "*.csv"); do 
 	echo "'$file' -> db" ; 
 	mysql -u root --password="" \
-	-e "set @file='$file', @table='owl2019'; source scripts/sql/old_players_data.sql"
+	-e "set @file='$file', @table='owl2019'; source ~/scripts/sql/old_players_data.sql"
 done
 
 echo "Moving 2020_1 to db" ; 
 mysql -u root --password="" \
-	-e "set @file='~/owl_data/2020/phs_2020_1.csv', @table='owl2020'; source scripts/sql/2020_1.sql"
+	-e "set @file='~/owl_data/2020/phs_2020_1.csv', @table='owl2020'; source ~/scripts/sql/2020_1.sql"
 
 echo "Moving 2020_2 to db" ; 
 mysql -u root --password="" \
-	-e "set @file='~/owl_data/2020/phs_2020_2.csv', @table='owl2020'; source scripts/sql/new_players_data.sql"
+	-e "set @file='~/owl_data/2020/phs_2020_2.csv', @table='owl2020'; source ~/scripts/sql/new_players_data.sql"
 
 for file in $(find ~/owl_data/2021 -name "*.csv"); do 
 	echo "'$file' -> db" ; 
 	mysql -u root --password="" \
-	-e "set @file='$file', @table='owl2021''; source scripts/sql/new_players_data.sql"
+	-e "set @file='$file', @table='owl2021''; source ~/scripts/sql/new_players_data.sql"
 done
 
 echo "Done moving files"
