d3319a452a0fdbf37cab2c0d86fc70890ffe967e
louismeunier <louismeunier123@gmail.com>
2021-04-25 17:39:34 -0400

    Change id

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -5,13 +5,13 @@ mysql -u root --password="" < scripts/sql/init_db.sql
 # input the data
 
 for f in $(find ~/owl_data/2018 -name "*.csv"); do 
-	export file = $f
+	export file=$f
 	mysql -u root --password="" \
 	-e "set @file='${file}', @table='owl2018'; source ~/scripts/sql/old_players_data.sql"
 done
 
 for f in $(find ~/owl_data/2019 -name "*.csv"); do 
-	export file = $f
+	export file=$f
 	mysql -u root --password="" \
 	-e "set @file='${file}', @table='owl2019'; source ~/scripts/sql/old_players_data.sql"
 done
@@ -25,7 +25,7 @@ mysql -u root --password="" \
 	-e "set @file='~/owl_data/2020/phs_2020_2.csv', @table='owl2020'; source ~/scripts/sql/new_players_data.sql"
 
 for f in $(find ~/owl_data/2021 -name "*.csv"); do 
-	export file = $f
+	export file=$f
 	mysql -u root --password="" \
 	-e "set @file='${file}', @table='owl2021''; source ~/scripts/sql/new_players_data.sql"
 done
