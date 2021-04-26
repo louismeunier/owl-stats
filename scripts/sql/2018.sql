af2c2bf8d21ac37974b9d2627219a6edbae30cd6
louismeunier <louismeunier123@gmail.com>
2021-04-25 20:53:57 -0400

    Remove dynamic table names

---
+++ scripts/sql/2018.sql
@@ -0,0 +1,15 @@
+USE OWL;
+LOAD DATA INFILE @file
+INTO TABLE owl2018
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+ESCAPED BY '\\'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@start_time,@match_id,@stage,map_type,map_name,@player,@team,stat_name,@hero,stat_amount)
+SET start_time = STR_TO_DATE(@start_time, '%m/%d/%Y %H:%i'),
+esports_match_id=@match_id,
+tournament_title=@stage,
+player_name=@player,
+team_name=@team,
+hero_name=@hero;
--- scripts/sql/2021.sql
+++ scripts/sql/2021.sql
@@ -1,10 +1,10 @@
 USE OWL;
 LOAD DATA INFILE @file
-INTO TABLE owl2020
+INTO TABLE @table
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 ESCAPED BY '\\'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (@start_time,esports_match_id,tournament_title,map_type,map_name,player_name,team_name,stat_name,hero_name,stat_amount)
-SET start_time = STR_TO_DATE(@start_time, '%m/%d/%Y %H:%i');
+SET start_time = STR_TO_DATE(@start_time, '%Y-%m-%d %H:%i:%s');
