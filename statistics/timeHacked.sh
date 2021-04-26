7566cec473f92d998874e47e0fcc8e5ecbb0aa39
louismeunier <louismeunier123@gmail.com>
2021-04-25 21:56:21 -0400

    Change paths to out dir

--- statistics/timeHacked.sh
+++ statistics/timeHacked.sh
@@ -3,6 +3,6 @@ mysql -u root --password="" OWL -t -e \
   SELECT SUM(stat_amount) as sum, player_name as player
   FROM owl2021 WHERE stat_name='Melee Kills' 
   GROUP BY player_name) as sum_ranks
-  ORDER BY sum desc;" > ~/out/mostmeleekills.md
+  ORDER BY sum desc;" > /home/travis/build/louismeunier/owl-stats/out/mostmeleekills.md
 
-./statistics/convertMD.sh ~/out/mostmeleekills.md
+./statistics/convertMD.sh /home/travis/build/louismeunier/owl-stats/out/mostmeleekills.md
