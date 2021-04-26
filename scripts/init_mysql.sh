2bd8c4cfe29fb90539908ddf352961f83dbb5f3d
louismeunier <louismeunier123@gmail.com>
2021-04-25 21:47:28 -0400

    Add logging etc

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -3,25 +3,27 @@
 mysql -u root --password="" < scripts/sql/init_db.sql
 
 # input the data
-
+echo "Moving 2018..."
 for f in $(find ~/owl_data/2018 -name "*.csv"); do 
 	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2018.sql
 done
 
+echo "Moving 2019..."
 for f in $(find ~/owl_data/2019 -name "*.csv"); do 
 	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2019.sql
 done
 
-echo "Moving 2020_1 to db" ; 
+echo "Moving 2020_1..." 
 sudo cp ~/owl_data/2020/phs_2020_1.csv /var/lib/mysql-files/current.csv
 mysql -u root --password="" < scripts/sql/2020_1.sql
 
-echo "Moving 2020_2 to db" ; 
+echo "Moving 2020_2..."
 sudo cp ~/owl_data/2020/phs_2020_2.csv /var/lib/mysql-files/current.csv
 mysql -u root --password="" < scripts/sql/2020_2.sql
 
+echo "Moving 2021..."
 for f in $(find ~/owl_data/2021 -name "*.csv"); do 
 	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2021.sql
