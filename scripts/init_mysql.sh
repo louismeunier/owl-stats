7068b465cf8ed15442aeab09ca2bcb8e1ba03317
louismeunier <louismeunier123@gmail.com>
2021-04-25 21:27:29 -0400

    Add sudo to copy

--- scripts/init_mysql.sh
+++ scripts/init_mysql.sh
@@ -5,25 +5,25 @@ mysql -u root --password="" < scripts/sql/init_db.sql
 # input the data
 
 for f in $(find ~/owl_data/2018 -name "*.csv"); do 
-	cp $f /var/lib/mysql-files/current.csv
+	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2018.sql
 done
 
 for f in $(find ~/owl_data/2019 -name "*.csv"); do 
-	cp $f ~/owl_data/current.csv
+	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2019.sql
 done
 
 echo "Moving 2020_1 to db" ; 
-cp ~/owl_data/2020/phs_2020_1.csv /var/lib/mysql-files/current.csv
+sudo cp ~/owl_data/2020/phs_2020_1.csv /var/lib/mysql-files/current.csv
 mysql -u root --password="" < scripts/sql/2020_1.sql
 
 echo "Moving 2020_2 to db" ; 
-cp ~/owl_data/2020/phs_2020_2.csv /var/lib/mysql-files/current.csv
+sudo cp ~/owl_data/2020/phs_2020_2.csv /var/lib/mysql-files/current.csv
 mysql -u root --password="" < scripts/sql/2020_2.sql
 
 for f in $(find ~/owl_data/2021 -name "*.csv"); do 
-	cp $f /var/lib/mysql-files/current.csv
+	sudo cp $f /var/lib/mysql-files/current.csv
 	mysql -u root --password="" < scripts/sql/2021.sql
 done
 
