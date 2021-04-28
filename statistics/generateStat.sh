ca22030ee893c461171e5d5f76205ecda9939be6
louismeunier <louismeunier123@gmail.com>
2021-04-27 22:59:59 -0400

    Fix file name written to

--- statistics/generateStat.sh
+++ statistics/generateStat.sh
@@ -3,7 +3,7 @@
 file_name=$1
 stat=$2
 
-echo '# ${stat} \n' >> /home/travis/build/louismeunier/owl-stats/statistics/temp.md
+echo '# ${stat} \n\n' >> /home/travis/build/louismeunier/owl-stats/out/"$file_name".md
 years=( 2021 2020 2019 2018 )
 for y in "${years[@]}"
 do
