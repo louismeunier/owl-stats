1779dfe771a0bf3eba8c3a22362957dae9bf179a
louismeunier <louismeunier123@gmail.com>
2021-05-01 10:34:39 -0400

    Fix formatting

--- statistics/stats.sh
+++ statistics/stats.sh
@@ -4,4 +4,4 @@ ruby ./statistics/logic/benched.rb
 ruby ./statistics/logic/timeHacked.rb
 
 today=$(date)
-echo "### last updated: ${today}\n\n" >> /home/travis/build/louismeunier/owl-stats/out/README.md
+echo "#### last updated: ${today}" >> /home/travis/build/louismeunier/owl-stats/out/README.md
