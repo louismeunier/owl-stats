3d73b5ed8bfcbcd93a227f62dd47d33446c49907
louismeunier <louismeunier123@gmail.com>
2021-05-01 10:29:41 -0400

    Add last updated day to README

--- statistics/stats.sh
+++ statistics/stats.sh
@@ -2,3 +2,6 @@
 
 ruby ./statistics/logic/benched.rb
 ruby ./statistics/logic/timeHacked.rb
+
+today=$(date)
+echo "### last updated: ${today}\n\n" >> /home/travis/build/louismeunier/owl-stats/out/README.md
